f1847c05fd8356387f12cb6beafdf7603daa95ef
Brayam Stiven Moreno <112638896+BrayamMoreno@users.noreply.github.com>
2025-08-10 23:26:25 -0500

    Feat: Gestion cuentas

--- src/main/java/com/pqrsdf/pqrsdf/controllers/UsuarioController.java
+++ src/main/java/com/pqrsdf/pqrsdf/controllers/UsuarioController.java
@@ -3,43 +3,71 @@ package com.pqrsdf.pqrsdf.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pqrsdf.pqrsdf.dto.UserRequest;
 import com.pqrsdf.pqrsdf.generic.GenericController;
 import com.pqrsdf.pqrsdf.models.Usuario;
 import com.pqrsdf.pqrsdf.service.RolService;
 import com.pqrsdf.pqrsdf.service.UsuarioService;
+import com.pqrsdf.pqrsdf.utils.ResponseEntityUtil;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping(path = "/api/usuarios")
 @Tag(name = "Gestion de Usuarios")
-public class UsuarioController extends GenericController<Usuario, Long>{
+public class UsuarioController extends GenericController<Usuario, Long> {
 
     private final PasswordEncoder passwordEncoder;
     private final RolService rolesService;
+    private final UsuarioService service;
 
     public UsuarioController(UsuarioService service, PasswordEncoder passwordEncoder,
-                            RolService rolesService){
+            RolService rolesService) {
         super(service);
         this.passwordEncoder = passwordEncoder;
         this.rolesService = rolesService;
+        this.service = service;
     }
 
+    @PostMapping("/disable-account")
+    public ResponseEntity<?> disableAccount(@RequestParam Long id) {
+        try {
+
+            if (id == null || id <= 0) {
+                return ResponseEntityUtil.handleBadRequest("ID de usuario inválido.");
+            }
+            service.disableAccount(id);
+            return ResponseEntity.ok("Cuenta deshabilitada correctamente.");
+        } catch (Exception e) {
+            return ResponseEntityUtil.handleInternalError(e);
+        }
+    }
+
+    @PostMapping("/enable-account")
+    public ResponseEntity<?> enableAccount(@RequestParam Long id) {
+        try {
+            if (id == null || id <= 0) {
+                return ResponseEntityUtil.handleBadRequest("ID de usuario inválido.");
+            }
+            service.enabbleAccount(id);
+            return ResponseEntity.ok("Cuenta habilitada correctamente.");
+        } catch (Exception e) {
+            return ResponseEntityUtil.handleInternalError(e);
+        }
+    }
 
     /**
-    @PostMapping("/user")
-    public ResponseEntity<?> createEntity(@RequestBody UserRequest request){
-        Usuario usuario = new Usuario();
-        usuario.setUsername(request.correo());
-        usuario.setContraseña(passwordEncoder.encode(request.contraseña()));
-        usuario.setPersona_id(request.personaId());
-        usuario.setRol(rolesService.getById(request.rolId()));
-        return super.createEntity(usuario);
-    }
-    */
+     * @PostMapping("/user")
+     * public ResponseEntity<?> createEntity(@RequestBody UserRequest request){
+     * Usuario usuario = new Usuario();
+     * usuario.setUsername(request.correo());
+     * usuario.setContraseña(passwordEncoder.encode(request.contraseña()));
+     * usuario.setPersona_id(request.personaId());
+     * usuario.setRol(rolesService.getById(request.rolId()));
+     * return super.createEntity(usuario);
+     * }
+     */
 }
--- src/main/java/com/pqrsdf/pqrsdf/service/UsuarioService.java
+++ src/main/java/com/pqrsdf/pqrsdf/service/UsuarioService.java
@@ -21,67 +21,85 @@ import jakarta.transaction.Transactional;
 @Service
 public class UsuarioService extends GenericService<Usuario, Long> {
 
-    private final UsuarioRepository usuarioRepository;
-    private final PersonaRepository personasRepository;
-    private final RolRepository rolesRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final TipoPersonaRepository tipoPersonaRepository;
-    private final GeneroRepository generoRepository;
-    private final TipoDocRepository tipoDocRepository;
-    private final MunicipioRepository municipioRepository;
+        private final UsuarioRepository usuarioRepository;
+        private final PersonaRepository personasRepository;
+        private final RolRepository rolesRepository;
+        private final PasswordEncoder passwordEncoder;
+        private final TipoPersonaRepository tipoPersonaRepository;
+        private final GeneroRepository generoRepository;
+        private final TipoDocRepository tipoDocRepository;
+        private final MunicipioRepository municipioRepository;
 
-    public UsuarioService(UsuarioRepository repository, PersonaRepository personasRepository,
-            RolRepository rolesRepository, PasswordEncoder passwordEncoder, TipoPersonaRepository tipoPersonaRepository,
-            GeneroRepository generoRepository, TipoDocRepository tipoDocRepository, MunicipioRepository municipioRepository) {
-        super(repository);
-        this.usuarioRepository = repository;
-        this.personasRepository = personasRepository;
-        this.rolesRepository = rolesRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.tipoPersonaRepository = tipoPersonaRepository;
-        this.generoRepository = generoRepository;
-        this.tipoDocRepository = tipoDocRepository;
-        this.municipioRepository = municipioRepository;
-    }
+        public UsuarioService(UsuarioRepository repository, PersonaRepository personasRepository,
+                        RolRepository rolesRepository, PasswordEncoder passwordEncoder,
+                        TipoPersonaRepository tipoPersonaRepository,
+                        GeneroRepository generoRepository, TipoDocRepository tipoDocRepository,
+                        MunicipioRepository municipioRepository) {
+                super(repository);
+                this.usuarioRepository = repository;
+                this.personasRepository = personasRepository;
+                this.rolesRepository = rolesRepository;
+                this.passwordEncoder = passwordEncoder;
+                this.tipoPersonaRepository = tipoPersonaRepository;
+                this.generoRepository = generoRepository;
+                this.tipoDocRepository = tipoDocRepository;
+                this.municipioRepository = municipioRepository;
+        }
 
-    public Usuario getEntityByCorreo(String Correo) {
-        return usuarioRepository.findByCorreo(Correo).orElse(null);
-    }
+        public Usuario getEntityByCorreo(String Correo) {
+                return usuarioRepository.findByCorreo(Correo).orElse(null);
+        }
 
-    @Transactional
-    public Usuario createNewUser(RegisterRequest entity) {
+        @Transactional
+        public Usuario createNewUser(RegisterRequest entity) {
 
-        usuarioRepository.findByCorreo(entity.correo()).ifPresent(u -> {
-            throw new RuntimeException("El correo ya se encuentra registrado");
-        });
+                usuarioRepository.findByCorreo(entity.correo()).ifPresent(u -> {
+                        throw new RuntimeException("El correo ya se encuentra registrado");
+                });
 
-        Persona persona = Persona.builder()
-                .nombre(entity.nombre())
-                .apellido(entity.apellido())
-                .tipoDoc(tipoDocRepository.findById(entity.tipoDocumento())
-                        .orElseThrow(() -> new RuntimeException("Tipo de documento no encontrado")))
-                .dni(entity.dni())
-                .tipoPersona(tipoPersonaRepository.findById(entity.tipoPersona())
-                        .orElseThrow(() -> new RuntimeException("Tipo de persona no encontrado")))
-                .telefono(entity.telefono())
-                .direccion(entity.direccion())
-                .municipio(municipioRepository.findById(entity.municipioId())
-                        .orElseThrow(() -> new RuntimeException("Municipio no encontrado")))
-                .genero(generoRepository.findById(entity.genero())
-                        .orElseThrow(() -> new RuntimeException("Género no encontrado")))
-                .build();
+                Persona persona = Persona.builder()
+                                .nombre(entity.nombre())
+                                .apellido(entity.apellido())
+                                .tipoDoc(tipoDocRepository.findById(entity.tipoDocumento())
+                                                .orElseThrow(() -> new RuntimeException(
+                                                                "Tipo de documento no encontrado")))
+                                .dni(entity.dni())
+                                .tipoPersona(tipoPersonaRepository.findById(entity.tipoPersona())
+                                                .orElseThrow(() -> new RuntimeException(
+                                                                "Tipo de persona no encontrado")))
+                                .telefono(entity.telefono())
+                                .direccion(entity.direccion())
+                                .municipio(municipioRepository.findById(entity.municipioId())
+                                                .orElseThrow(() -> new RuntimeException("Municipio no encontrado")))
+                                .genero(generoRepository.findById(entity.genero())
+                                                .orElseThrow(() -> new RuntimeException("Género no encontrado")))
+                                .build();
 
-        persona = personasRepository.save(persona);
+                persona = personasRepository.save(persona);
 
-        Rol rol = rolesRepository.findById(new Long(1)).orElse(null);
+                Rol rol = rolesRepository.findById(new Long(1)).orElse(null);
 
-        Usuario usuario = Usuario.builder()
-                .correo(entity.correo())
-                .contrasena(passwordEncoder.encode(entity.contraseña()))
-                .persona(persona)
-                .rol(rol)
-                .build();
+                Usuario usuario = Usuario.builder()
+                                .correo(entity.correo())
+                                .contrasena(passwordEncoder.encode(entity.contraseña()))
+                                .persona(persona)
+                                .rol(rol)
+                                .build();
 
-        return usuarioRepository.save(usuario);
-    }
+                return usuarioRepository.save(usuario);
+        }
+
+        public void disableAccount(Long id) {
+                Usuario usuario = usuarioRepository.findById(id)
+                                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+                usuario.setIsEnable(false);
+                usuarioRepository.save(usuario);
+        }
+
+        public void enabbleAccount(Long id) {
+                Usuario usuario = usuarioRepository.findById(id)
+                                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+                usuario.setIsEnable(true);
+                usuarioRepository.save(usuario);
+        }
 }
