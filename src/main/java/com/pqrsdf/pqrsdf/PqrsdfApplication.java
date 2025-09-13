a261e3ea5030153797064599b8177a5c591248c4
Brayam Stiven Moreno <brayanmoreno650@gmail.com>
2025-09-12 21:23:29 -0500

    Feat: Despliegue

--- src/main/java/com/pqrsdf/pqrsdf/PqrsdfApplication.java
+++ src/main/java/com/pqrsdf/pqrsdf/PqrsdfApplication.java
@@ -9,12 +9,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class PqrsdfApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-		System.setProperty("FILE_DIR", dotenv.get("FILE_DIR"));
-
 		SpringApplication.run(PqrsdfApplication.class, args);
 	}
 }
